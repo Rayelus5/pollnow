922dac198e1bd16565ddba1317b32430aeba671a
Rayelus5 <rayelusanom828@gmail.com>
2026-02-09 13:04:32 +0100

    [+] new ads (chaotic loom colab)

--- src/components/ads/CustomAdBanner.tsx
+++ src/components/ads/CustomAdBanner.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export const CustomAdBanner = () => {
     return (
         <Link href="/about#contact">
-            <img src="/ads/Horizontal.jpg" alt="Publicidad banner" className="w-full h-auto" />
+            <img src="/ads/Horizontal-1.webp" alt="Publicidad banner" className="w-full h-auto" />
         </Link>
     );
 };
