51cf914bf5235b9b4c739a82158b185367914215
Rayelus5 <rayelusanom828@gmail.com>
2026-02-09 13:09:33 +0100

    [-] new ad link

--- src/components/ads/CustomAdBanner.tsx
+++ src/components/ads/CustomAdBanner.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export const CustomAdBanner = () => {
     return (
-        <Link href="/about#contact">
+        <Link href="https://chaotic-loom.com" target="_blank">
             <img src="/ads/Horizontal-1.webp" alt="Publicidad banner" className="w-full h-auto" />
         </Link>
     );
--- src/components/ads/CustomAdBannerVertical.tsx
+++ src/components/ads/CustomAdBannerVertical.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export const CustomAdBannerVertical = () => {
     return (
-        <Link href="/about#contact">
+        <Link href="https://chaotic-loom.com" target="_blank">
             <img src="/ads/Vertical-1.webp" alt="Publicidad banner" className="w-full h-auto" />
         </Link>
     );
